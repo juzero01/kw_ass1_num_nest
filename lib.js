b7e26ec7cb0f46bba22ddd317310acde0caddd39
kim-juyoung <wndud4901@gmail.com>
2024-11-02 15:27:13 +0900

    0.0.2 commit

--- lib.js
+++ lib.js
@@ -6,12 +6,20 @@ function avg(numbers) {
     return s / numbers.length;
 }
 function prime(num) {
-
-    return false;
+    if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false; // If num is divisible by any number other than 1 and itself
+    }
+    return true;
 }
-function factorial(num) {
-
-    return -1;
+function factorial(num) { //up to 15
+    if (num < 0) return -1; // Factorial is not defined for negative numbers
+    if (num > 15) return -1; // As requested, limit factorial to numbers up to 15
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 module.exports = {
--- lib.test.js
+++ lib.test.js
@@ -6,3 +6,23 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+test("prime(7) should be true", () => {
+    expect(lib.prime(7)).toBe(true);
+});
+test("prime(10) should be false", () => {
+    expect(lib.prime(10)).toBe(false);
+});
+test("prime(1) should be false", () => {
+    expect(lib.prime(1)).toBe(false);
+});
+
+// Test for factorial function
+test("factorial(5) should be 120", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
+test("factorial(0) should be 1", () => {
+    expect(lib.factorial(0)).toBe(1);
+});
+test("factorial(16) should be -1 (as it's over the limit)", () => {
+    expect(lib.factorial(16)).toBe(-1);
+});
